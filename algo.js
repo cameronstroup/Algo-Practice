1a71f905a53391d0f80a84ed2fd2a885caa4f044
cameronstroup <90347622+cameronstroup@users.noreply.github.com>
2022-04-22 14:33:17 -0500

    algo

---
+++ algo.js
@@ -0,0 +1,90 @@
+// const { isNumberObject } = require("util/types");
+
+let vals = [5, 4, 1, 2, 9, 55, 34, 2, 5, 66];
+
+// // let newV = vals.map((num) => (num / 2) & 0);
+// // console.log(newV);
+
+// // npm cache verify
+// // // // function sum(acc, val) {
+// // // //   console.log(acc);
+// // // //   return acc + val;
+// // // // }
+// // // // let answer = vals.reduce(sum, 10);
+// // // // console.log(answer);
+
+// // // max = (acc, val) => {
+// // //   if ((acc = val)) {
+// // //     return acc;
+// // //   }
+// // // // };
+
+// // // function repatStr(n, s) {
+// // //   return s.repeat(n);
+// // // }
+// // // console.log(repatStr(10, "whats up"));
+
+// // function invert(array) {
+// //   return array.map((num) => num * 2);
+// // }
+// // newArray = invert(array);
+
+// // (newArray) => newArray.reduce((con, value) => value + con);
+
+// // console.log(newArray);
+
+// // function TcuPlayer(posistion, number, firstName, lastName, year) {
+// //   this.posistion = posistion;
+// //   this.number = number;
+// //   this.firstName = firstName;
+// //   this.lastName = lastName;
+// //   this.year = year;
+// // }
+
+// // let MikeMiles = new TcuPlayer("PG", 10, "Mike", "Miles", "JR");
+
+// // console.log(MikeMiles);
+
+// // function DOG(age, breed, goodBoy, age) {
+// //   this.age = age;
+// //   this.breed = breed;
+// //   this.goodBoy = goodBoy;
+// //   this.age = age;
+// // }
+
+// // let Lake = new DOG(4, "lab", "yes", 28);
+
+// // console.log(Lake);
+// let newVals = vals.map((num) => num * 2);
+// console.log(newVals);
+
+// 'Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case r, you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following names:'
+
+// //
+
+// let areYouPlayingBanjo = (name) => {
+//   if (name[0] === r || name[0] === R) {
+//     return  `${name'
+//   } else {
+//     return "${name}  does not play banjo";
+// //   }
+// function opposite(a) {
+//   return a + 1;
+//   //your code here
+// }
+// console.log(opposite(5));
+
+// function find_average(array) {
+//   let sum = 0;
+//   if ((array = [] || NaN)) {
+//     return 0;
+//   } else {
+//     for (let i = 0; i < array.length; i++) {
+//       sum = sum + array[i];
+//     }
+//     return sum / array.length;
+//   }
+// }
